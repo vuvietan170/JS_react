5e9a99270412a3374430313c3e299964ae03d3bb
TanVu170 <vuvietan17102005@gmail.com>
2026-06-26 15:08:21 +0700

    feat(bai2): add bai2 from 01

--- JS/exercise_1.js
+++ JS/exercise_1.js
@@ -8,3 +8,10 @@ let factorial = n => {
 }
 console.log(factorial(5));
 
+//bai2
+for (let i = 1; i<=9; i++){
+    console.log(`Bang cuu chuong ${i}`)
+        for(let j = 1; j<= 10;j++){
+            console.log(`${i}*${j} = ${i*j}`)
+        }
+}
