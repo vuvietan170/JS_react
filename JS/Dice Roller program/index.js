ce2c07edb3d1e3b95397ac42d0867f82a65713e5
TanVu170 <vuvietan17102005@gmail.com>
2026-07-02 01:36:24 +0700

    Dice Roller program

---
+++ JS/Dice Roller program/index.js
@@ -0,0 +1,15 @@
+function rollDice(){
+    const numDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("DiceResult");
+    const diceImages = document.getElementById("DiceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numDice;i++){
+        const value = Math.floor(Math.random()*6) + 1;
+        values.push(value);
+        images.push(`<img src="images/${value}.png" alt="Dice ${value}">` );
+    }
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+}
